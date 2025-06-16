d1928ca528f5c2a7788a39e79209a0656f34c4d3
Christian Raue <captain_2@gmx.de>
2025-06-16 10:57:25 +0200

    changed title images

--- app/pages/init.py
+++ app/pages/init.py
@@ -1,3 +1,5 @@
+import os
+from itertools import count
 import streamlit as st
 from app.pages.shared import render_image, scroll_to_top
 from app.localization import Localization
@@ -6,8 +8,18 @@ from app.state import QuizState
 
 def render_init():
     st.header(Localization.get('intro').format(number=len(st.session_state['questions'])))
+    st.divider()
+    for i in count():
+        png_filename = f"title{i}_{Localization.language()}.png"
+        jpg_filename = f"title{i}_{Localization.language()}.jpg"
+        if os.path.exists(os.path.join("images", png_filename)):
+            render_image(png_filename)
+        elif os.path.exists(os.path.join("images", jpg_filename)):
+            render_image(jpg_filename)
+        else:
+            break
+    st.divider()
 
-    render_image(f"title_{Localization.language()}.jpg")
     left_column, right_column = st.columns(spec=[0.9, 0.1])
 
     with left_column:
