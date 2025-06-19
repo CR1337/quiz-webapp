27208d2bbe33e20069a010686d3bef26a180e43f
Christian Raue <captain_2@gmx.de>
2025-06-19 07:58:41 +0200

    fixed bug

--- app/colors.py
+++ app/colors.py
@@ -8,7 +8,7 @@ Color = Tuple[int, int, int]
 class Colors:
     COLORS_FILENAME: str = os.path.join("data", "colors.json")
     with open(COLORS_FILENAME, "r") as file:
-        _colors: Dict[str, Color] = {k: tuple(c) for k, c in json.load(file)}
+        _colors: Dict[str, Color] = {k: tuple(c) for k, c in json.load(file).items()}
 
     @classmethod
     def get(cls, key: str) -> Color:
