566330b0b26a72d0c448fd1530fbc795ab85012a
CR1337 <captain_2@gmx.de>
2026-09-08 10:59:01 +0200

    added image conversion tool

---
+++ tools/jpg_to_png.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import sys
+import os
+
+
+if len(sys.argv) < 2:
+    print("You must specify a jpg file!", file=sys.stderr)
+    exit(1)
+
+in_filename = sys.argv[1]
+
+if not in_filename.endswith(".jpg") and not in_filename.endswith(".jpeg"):
+    print("You must specify a jpg file!", file=sys.stderr)
+    exit(1)
+
+out_filename = ".".join(in_filename.split(".")[:-1])
+
+image = Image.open(in_filename)
+image.save(out_filename)
+
+os.remove(in_filename)
